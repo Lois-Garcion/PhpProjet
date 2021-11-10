785159c51d0a95d353e08aad3bf89c7bcafef661
Florian Cam <florian.cam@etu.umontpellier.fr>
2021-11-10 15:42:47 +0100

    Je push tu connais

--- web/view/Produit/ListProduit.php
+++ web/view/Produit/ListProduit.php
@@ -1,10 +1,23 @@
-<?php
-if($tab_produit == false){
-    echo "Il n'y a pas de produit";
-}
-else{
-    foreach ($tab_produit as $v){
-        echo "<p>".$v->getNomProduit(). " d'id : ".$v->getIdProduit(). " , de prix : ".$v->getPrix() . " , de categorie : ".$v->getCategorie();
+<div class="head">
+    <h1>Nos produits</h1>
+    <hr/>
+</div>
+<section class = "products">
+    <?php
+    if($tab_produit == false){
+        echo "Il n'y a pas de produit";
     }
-}
-
+    foreach ($tab_produit as $v){
+        echo "
+            <div class = \"product-card\">
+                <div class = \"product-image\">
+                    <img src =\"assets/images/raquette-yonex.jpg\">
+                </div>
+                <div class = \"product-info\">
+                    <h5>" . $v->getNomProduit() . "</h5>
+                    <h6>" . $v->getPrix() ."€". "<h6>
+                </div>
+            </div>";
+    }
+    ?>
+</section>
