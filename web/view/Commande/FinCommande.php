7d406654f6a47fed5022eb767c7fba4f293a7e3d
Florian Cam <florian.cam@etu.umontpellier.fr>
2021-12-06 22:51:27 +0100

    Changement liste produit commande

--- web/view/Commande/FinCommande.php
+++ web/view/Commande/FinCommande.php
@@ -1,6 +1,6 @@
 <div class="validation-order">
     <h1>Merci de votre commande !</h1>
     <img src="assets/images/validation.jpg">
-    <h2>Nous l'avons bien reçu et nous la traiterons dans les plus brefs délais</h2>
+    <h2>Nous l'avons bien reçue et nous la traiterons dans les plus brefs délais</h2>
     <button onclick="location.href='?controller=ControllerCommande&action=readAll'">Voir la liste des commandes</button>
 </div>
--- web/view/Commande/listProduitCommande.php
+++ web/view/Commande/listProduitCommande.php
@@ -8,7 +8,7 @@ else {
     echo"<ul class=\"responsive-table\">";
     echo"
         <li class=\"table-header\">
-      <div class=\"col col-1\">ID</div>
+      <div class=\"col col-1\">Produit</div>
       <div class=\"col col-2\">Nom produit</div>
       <div class=\"col col-3\">Prix</div>
       <div class=\"col col-4\">Quantité</div>
@@ -18,7 +18,7 @@ else {
         $produit = Produit::getById($v->getIdProduit());
         echo "
         <li class=\"table-row\">
-              <div class=\"col col-1\" data-label=\"id\">" . $v->getIdProduit() . "</div>
+              <div class=\"col col-1\" data-label=\"id\"><img src =" . $produit->getFilepath() . "></div>   
               <div class=\"col col-2\" data-label=\"nom\">" . $produit->getNomProduit() . "</div>
               <div class=\"col col-3\" data-label=\"prix\">" . $produit->getPrix()*$v->getQuantite() . "€" . "</div>
               <div class=\"col col-4\" data-label=\"quantite\">" . $v->getQuantite() . "</div>
